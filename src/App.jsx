eeb4f4a09814db534517df3413bd596e847524cd
Ahsaan099 <160276367+Ahsaan099@users.noreply.github.com>
2026-04-14 02:08:35 +0500

    fix: add default route to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import One from './one';
 import Second from './second';
 import Navbar from './Navbar';
+
 const App = () => {
   return (
     <>
-<Navbar/>
-<Routes>
-  <Route path='/one' element={<One/>}/>
-    <Route path='/second' element={<Second/>}/>
-</Routes>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<One/>}/>
+        <Route path='/one' element={<One/>}/>
+        <Route path='/second' element={<Second/>}/>
+      </Routes>
     </>
   )
 }
+
 export default App;
